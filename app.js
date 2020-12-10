c121dffce9f8d0bd323a3756026ab915ef0e65b3
Xis <rick-s@hotmail.com>
2020-12-09 23:58:39 -0800

    department function done working on employee function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const connection = require("./assets/js/connection");
 const inquirer = require("inquirer");
-const questions = require("/assets/js/questions")
+const questions = require("./assets/js/questions");
 
 // Start our application
 init();
@@ -9,25 +9,25 @@ async function init() {
   const { action } = await inquirer.prompt(questions);
   switch (action) {
     case "Edit Department":
-      specificArtist();
+      editDepartments();
       break;
     case "Edit Role":
-      multiSearch();
+      // multiSearch();
       break;
     case "Edit Employee":
-      rangeSearch();
+      editEmployee();
       break;
     case "View All Employees":
-      specificSong();
+      removeEmployee();
       break;
     case "Search Employees By Manager":
-      songAndAlbumSearch();
+      // songAndAlbumSearch();
       break;
     case "Update Employee Managers":
-      songAndAlbumSearch();
+      // songAndAlbumSearch();
       break;
     case "Total Budget By Department":
-      songAndAlbumSearch();
+      // songAndAlbumSearch();
       break;
     case "Exit":
       process.exit(0);
@@ -37,19 +37,182 @@ async function init() {
   }
 }
 
-// * A query which returns all data for songs sung by a specific artist
-async function specificArtist() {
-  const { artist } = await inquirer.prompt({
-    name: "artist",
-    type: "input",
-    message: "What artist would you like to search for?",
+// starts department selection process
+async function editDepartments() {
+  const { department } = await inquirer.prompt({
+    name: "department",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Add Department", "Remove Department", "Exit"],
   });
 
-  const query = "SELECT position, song, year FROM top5000 WHERE ?";
-  // SELECT position, song, year FROM top5000 WHERE artist = "Led Zeppelin"
-  const data = await connection.query(query, { artist });
-  console.table(data);
-  init();
+  if (department === "Add Department") {
+    addDepartment();
+  } else if (department === "Remove Department") {
+    removeDepartment();
+  } else {
+    init();
+  }
+}
+
+async function addDepartment() {
+  const departmentName = await inquirer.prompt({
+    name: "department",
+    type: "input",
+    message: "What is the department name",
+  });
+
+  const data = departmentName.department;
+
+  const query = await connection.query(
+    "INSERT INTO department SET ?",
+    {
+      dept: data,
+    },
+
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " -- New Department Added --\n");
+      // Call updateProduct AFTER the INSERT completes
+      init();
+    }
+  );
+}
+
+async function removeDepartment() {
+  connection.query(
+    "SELECT dept AS departments FROM department",
+    async function (err, departments) {
+      const data = await inquirer.prompt([
+        {
+          name: "departments",
+          message: "What department would you like to remove?",
+          type: "list",
+          choices: departments.map((department) => ({
+            name: department.departments,
+          })),
+        },
+      ]);
+      connection.query("DELETE FROM department WHERE ?", {
+        dept: data.departments,
+      });
+      console.log(res.affectedRows + " -- Department Removed --\n");
+      // Call updateProduct AFTER the INSERT completes
+      init();
+    }
+  );
+}
+async function editEmployee() {
+  const { employee } = await inquirer.prompt({
+    name: "employee",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Add Employee", "Remove Employee", "Exit"],
+  });
+
+  if (employee === "Add Employee") {
+    addEmployee();
+  } else if (employee === "Remove Employee") {
+    removeEmployee();
+  } else {
+    init();
+  }
+}
+async function addEmployee() {
+  const employeeName = await inquirer.prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "What is the employees first name?",
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the employees last name?",
+    },
+    {
+      name: "roleID",
+      type: "list",
+      message: "What is the employees role?",
+      choices: [
+        "Intern",
+        "Engineer",
+        "Code Wizard",
+        "Cat Herder",
+        "OnlyFans/Self Employeed",
+      ],
+    },
+    {
+      name: "managerID",
+      type: "confirm",
+      message: "Is this person a manager",
+    },
+  ]);
+  switch (employeeName.roleID) {
+    case "Intern":
+      employeeName.roleID = 1;
+      break;
+    case "Engineer":
+      employeeName.roleID = 2;
+      break;
+    case "Code Wizard":
+      employeeName.roleID = 3;
+      break;
+    case "Cat Herder":
+      employeeName.roleID = 4;
+      break;
+    case "OnlyFans/Self Employeed":
+      employeeName.roleID = 5;
+      break;
+  }
+  switch (employeeName.managerID) {
+    case true:
+      employeeName.managerID = 1;
+      break;
+    case false:
+      employee.managerID = 0;
+      break;
+  }
+
+  const query = await connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      first_name: employeeName.firstName,
+      last_name: employeeName.lastName,
+      role_id: employeeName.roleID,
+      manager_id: employeeName.managerID,
+    },
+
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " -- New Employee Added --\n");
+      init();
+    }
+  );
+}
+// this is where the remove employee function will go.
+async function removeDepartment() {
+  connection.query(
+    "SELECT dept AS departments FROM department",
+    async function (err, departments) {
+      const data = await inquirer.prompt([
+        {
+          name: "departments",
+          message: "What department would you like to remove?",
+          type: "list",
+          choices: departments.map((department) => ({
+            name: department.departments,
+          })),
+        },
+      ]);
+      connection.query("DELETE FROM department WHERE ?", {
+        dept: data.departments,
+      });
+      console.log(res.affectedRows + " -- Department Removed --\n");
+      // Call updateProduct AFTER the INSERT completes
+      init();
+    }
+  );
 }
 
 // * A query which returns all artists who appear within the top 5000 more than once
@@ -105,24 +268,6 @@ async function specificSong() {
   const data = await connection.query("SELECT * FROM top5000 WHERE ?", {
     song,
   });
-  console.table(data);
-  init();
-}
-
-async function songAndAlbumSearch() {
-  const { artist } = await inquirer.prompt({
-    name: "artist",
-    type: "input",
-    message: "What artist would you like to search for?",
-  });
-  const query = `
-  SELECT top5000.artist, top5000.song, top_albums.year, top_albums.album, top_albums.position
-  FROM top5000 INNER JOIN top_albums
-  ON top_albums.artist = top5000.artist AND top_albums.year = top5000.year
-  WHERE top_albums.artist = ? AND top5000.artist = ?
-  ORDER BY top_albums.position`;
-
-  const data = await connection.query(query, [artist, artist]);
   console.table(data);
   init();
 }
